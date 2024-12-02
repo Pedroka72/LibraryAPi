60721a0d8e3db722cd9d6504d232792c9bd7dc9a
GabrielAFds <143099311+GabrielAFds@users.noreply.github.com>
2024-12-01 22:59:04 -0300

    Emprestimo.js

--- Libraryapi/models/Emprestimo.js
+++ Libraryapi/models/Emprestimo.js
@@ -1,41 +1,49 @@
 module.exports = (sequelize, DataTypes) => {
-    const Emprestimo = sequelize.define('Emprestimos', {
+  const Emprestimo = sequelize.define(
+    "Emprestimos",
+    {
+      dataEmprestimo: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      dataDevolucao: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: "pendente",
+      },
+      usuarioId: {
+        type: DataTypes.INTEGER,
+        allowNull: true, // Permitir NULL para suportar SET NULL
+        references: {
+          model: "Usuarios", // Nome da tabela relacionada
+          key: "id",
+        },
+        onDelete: "SET NULL", // Define o comportamento ao excluir o usuário
+        onUpdate: "CASCADE",
+      },
+      livroId: {
+        type: DataTypes.INTEGER,
+        allowNull: true, // Permitir NULL para suportar SET NULL
+        references: {
+          model: "Livros", // Nome da tabela relacionada
+          key: "id",
+        },
+        onDelete: "SET NULL", // Define o comportamento ao excluir o livro
+        onUpdate: "CASCADE",
+      },
+    },
+    {
+      tableName: "Emprestimos",
+    }
+  );
 
-        dataEmprestimo: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        dataDevolucao: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        status: {
-            type: DataTypes.STRING,
-            defaultValue: 'pendente'
-        },
-        usuarioId: {
-            type: DataTypes.INTEGER,
-            allowNull: true, // Permitir NULL para suportar SET NULL
-            references: {
-                model: 'Usuarios', // Nome da tabela relacionada
-                key: 'id'
-            },
-            onDelete: 'SET NULL', // Define o comportamento ao excluir o usuário
-            onUpdate: 'CASCADE'
-        },
-        livroId: {
-            type: DataTypes.INTEGER,
-            allowNull: true, // Permitir NULL para suportar SET NULL
-            references: {
-                model: 'Livros', // Nome da tabela relacionada
-                key: 'id'
-            },
-            onDelete: 'SET NULL', // Define o comportamento ao excluir o livro
-            onUpdate: 'CASCADE'
-        }
-    }, {
-        tableName: 'Emprestimos'
-    });
+  Emprestimo.associate = (models) => {
+    Emprestimo.belongsTo(models.Livro, { foreignKey: 'livroId' });
+        Emprestimo.belongsTo(models.Usuario, { foreignKey: 'usuarioId' });
+  };
 
-    return Emprestimo;
+  return Emprestimo;
 };
